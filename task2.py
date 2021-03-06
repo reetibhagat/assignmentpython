62e26ab17cfbc122cb4fcf6cecbfe5dd43f846f4
Reeti Bhagat <bhagat.reeti@gmail.com>
2021-03-06 12:25:45 -0800

    added task 2 assignment

---
+++ task2.py
@@ -0,0 +1,86 @@
+# TASK TWO
+# OPERATORS AND DECISION MAKING STATEMENT
+
+# 1.  Write a program in Python to perform the following operation:
+
+# If a number is divisible by 3 it should print “Consultadd” as a string
+# If a number is divisible by 5 it should print “Python Training” as a string
+# If a number is divisible by both 3 and 5 it should print “Consultadd - Python Training” as a
+# string.
+
+
+# num = int(input("enter the number:"))
+#
+#
+# def check_multiple(num):
+#     if num % 5 == 0 and num % 3 == 0:
+#         print("Consultadd - Python Training")
+#     elif num % 5 == 0:
+#         print("Python Training")
+#     elif num % 3 == 0:
+#         print("Consultadd")
+#     else:
+#         print(None)
+#
+#
+# (check_multiple(num))
+
+# 2. Write a program in Python to perform the following operator based task:
+# Ask user to choose the following option first:
+# If User Enter 1 - Addition
+# If User Enter 2 - Subtraction
+# If User Enter 3 - Division
+# If User Enter 4 - Multiplication
+# If User Enter 5 - Average
+# Ask user to enter two numbers and keep those numbers in variables num1 and num2
+# respectively for the first 4 options mentioned above.
+# Ask the user to enter two more numbers as first and second for calculating the average as
+# soon as the user chooses an option 5.
+# At the end if the answer of any operation is Negative print a statement saying “NEGATIVE”
+# NOTE: At a time a user can only perform one action.
+
+
+options = int(input("enter the options 1-5 :"))
+
+
+def operation(options):
+    global num1, num2, num3, num4
+    if options == 1 or options == 2 or options == 3 or options == 4 or options == 5:
+        num1 = int(input("enter the number :"))
+        num2 = int(input("enter the number :"))
+    elif options == 5:
+        num3 = int(input("enter the number :"))
+        num4 = int(input("enter the number :"))
+    if options == 1:
+        ans = (num1 + num2)
+        if ans > 0:
+            print(ans)
+        else:
+            print("NEGATIVE")
+    elif options == 2:
+        ans = (num1 - num2)
+        if ans > 0:
+            print(ans)
+        else:
+            print("NEGATIVE")
+    elif options == 3:
+        ans = (num1 / num2)
+        if ans > 0:
+            print(ans)
+        else:
+            print("NEGATIVE")
+    elif options == 4:
+        ans = (num1 * num2)
+        if ans > 0:
+            print(ans)
+        else:
+            print("NEGATIVE")
+    elif options == 5:
+        ans = (num1 + num2 + num3 + num4 / 4)
+        if ans > 0:
+            print(ans)
+        else:
+            print("NEGATIVE")
+
+
+operation(options)
