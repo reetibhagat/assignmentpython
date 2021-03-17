04f5ecbc240d2bcd4ded72ace31030592840fbb0
Reeti Bhagat <bhagat.reeti@gmail.com>
2021-03-16 22:26:42 -0700

    added task 3 assignments

---
+++ task3/task3_1.py
@@ -0,0 +1,4 @@
+# 1. Create a list of 10 elements of four different data types like int, string, complex and float.
+complex_list = [1, 'a', 23.9, True, complex(1, 3), 55, "reeti", False, 665.99]
+
+print(complex_list)
