5c47f7c298498450a07e67b9dc9b638d581a824b
Reeti Bhagat <bhagat.reeti@gmail.com>
2021-03-22 15:13:31 -0700

    added task 6 assignment

---
+++ task6/task6.2.py
@@ -0,0 +1,2 @@
+# 1. Write a program in Python to find out the character in a string which is uppercase using list
+# comprehension.
---
+++ task6/task6.3.py
@@ -0,0 +1,6 @@
+# 2. Write a program to construct a dictionary from the two lists containing the names of students and
+# their corresponding subjects. The dictionary should map the students with their respective subjects.
+# Let’s see how to do this using for loops and dictionary comprehension.
+#     HINT - Use Zip function also
+# Sample input: students = ['Smit', 'Jaya', 'Rayyan'] subjects = ['CSE', 'Networking', 'Operating System']
+# Expected output: {‘Smit’ : ’CSE’ , ’Jaya’ : ’Networking’ , ’Rayyan’ : ’Operating System’}
